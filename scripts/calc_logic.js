ebe787001bbd5e31715b7841b57ccd6c7fbc87cf
Connor <connorwhisman17@gmail.com>
2022-11-13 00:03:01 -0500

    Add comments and clean up code

--- scripts/calc_logic.js
+++ scripts/calc_logic.js
@@ -1,9 +1,73 @@
+// VARIABLES USED FOR CALCULATIONS
 var input1 = '0';
 var input2;
 var operator;
 
+// SET INITIAL OUTPUT TO '0'
 updateView(input1);
 
+// -------------------------------- LOGICAL FUNCTIONS --------------------------------------------
+// ADD OPERATOR TO OUTPUT (AND DO CALCULATION IF WE HAVE TWO INPUTS)
+function operate(symbol) {
+    // CALCULATE
+    if (operator !== undefined && input2 !== undefined) {
+        var pre_solve = input2;
+        input2 = switch_case();
+        log_calculation(pre_solve, operator, input1, input2);
+        updateView(input2);
+    }
+    // STORE INITIAL INPUT TO PREPARE FOR SECOND INPUT 
+    else {
+        input2 = input1;
+        updateView(input1);
+    }
+    // RESET AFTER EITHER CASE
+    input1 = '0';
+    operator = symbol;
+}
+
+// SOLVE IF EQUAL BUTTON IS PRESSED (RESET IF WE DONT HAVE AN OPERATOR AND BOTH INPUTS)
+function solve() {
+    if (operator == undefined) {
+        clear_output();
+        return
+    }
+    var pre_solve = input1;
+    input1 = switch_case();
+    updateView(input1);
+    log_calculation(input2, operator, pre_solve, input1);
+    operator = undefined;
+}
+
+// PERFORM CORRECT OPERATION DEPENDING ON WHICH BUTTON WAS PRESSED (CALLED IN 'OPERATE' AND 'SOLVE' FUNCTIONS)
+function switch_case() {
+    switch(operator) {
+        case '+':
+            return String(parseFloat(input2) + parseFloat(input1));
+        case '-':
+            return String(parseFloat(input2) - parseFloat(input1));
+        case '*':
+            return String(parseFloat(input2) * parseFloat(input1));
+        case '/':
+            return String(parseFloat(input2) / parseFloat(input1));
+    }
+}
+
+// ADDS ABILITY TO SWITCH BETWEEN POSITIVE AND NEGATIVE NUMBERS
+function inverter() {
+    if (input1 !== '0') {
+        input1 = input1 * (-1);
+        updateView(input1);
+    }
+}
+
+// KEEPS TRACK OF EVERY TIME A CALCULATION IS PERFORMED (CALLED IN 'OPERATE' AND 'SOLVE' FUNCTIONS)
+function log_calculation(n2, op, n1, sol) {
+    console.log(String(n2 + op + n1 + '=' + sol));
+}
+
+// ---------------------------- UPDATE / DISPLAY FUNCTIONS ---------------------------------------
+// ADD VALUES TO OUTPUT (CHECK FOR REPEATED DECIMAL INPUTS)
 function append_value(char) {
     if (input1.includes('.') && char == '.') return;
     if (input1 == '0') {
@@ -19,56 +83,7 @@ function append_value(char) {
     }
     updateView(input1);
 }
-
-function operate(symbol) {
-    if (input1 !== '0' && operator !== undefined && input2 !== undefined) {
-        var pre_solve = input2;
-        input2 = switch_case();
-        log_calculation(pre_solve, operator, input1, input2);
-        updateView(input2);
-    }
-    else {
-        input2 = input1;
-        updateView(input1);
-    }
-    input1 = '0';
-    operator = symbol;
-}
-
-function solve() {
-    if (operator == undefined) return;
-    var pre_solve = input1;
-    input1 = switch_case();
-    updateView(input1);
-    log_calculation(input2, operator, pre_solve, input1);
-    operator = undefined;
-}
-
-function switch_case() {
-    switch(operator) {
-        case '+':
-            return String(parseFloat(input2) + parseFloat(input1));
-        case '-':
-            return String(parseFloat(input2) - parseFloat(input1));
-        case '*':
-            return String(parseFloat(input2) * parseFloat(input1));
-        case '/':
-            return String(parseFloat(input2) / parseFloat(input1));
-    }
-}
-
-function inverter() {
-    if (input1 !== '0') {
-        input1 = input1 * (-1);
-        updateView(input1);
-    }
-}
-
-function log_calculation(n2, op, n1, sol) {
-    console.log(String(n2 + op + n1 + '=' + sol));
-}
-
-
+// RESETS CALCULATOR
 function clear_output() {
     input1 = '0';
     input2 = undefined;
@@ -76,6 +91,7 @@ function clear_output() {
     updateView(input1);
     console.log('Cleared.');
 }
+// CALLED EVERYTIME THE OUTPUT IS UPDATED (CHECKS FOR NON NUMBERS EX: 0/0=NaN, AND RESETS CALCULATOR)
 function updateView(show) {
     if (show == 'NaN') {
         clear_output();
--- scripts/menu.js
+++ scripts/menu.js
@@ -1,20 +1,13 @@
 // TOGGLE DROPDOWN AND ROTATE MENU ICON
 function myFunction() {
     document.getElementById("menu-icon").classList.toggle("rotate-menu");
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("dropdown").classList.toggle("show");
 }
   
 // IF USER CLICKS OUTSIDE OF DROPDOWN AREA, CLOSE THE MENU
 window.onclick = function(event) {
     if (!event.target.matches('#menu-icon')) {
-        var dropdowns = document.getElementsByClassName("menu-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
         document.getElementById("menu-icon").classList.remove('rotate-menu');
+        document.getElementById("dropdown").classList.remove("show");
     }
 }
